6d2cae5c02cccfc80fad754e2f9ed650f707ddc6
weichi <sweetherbe@gmail.com>
2020-10-07 11:39:36 +0800

    Updates

---
+++ precache-manifest.b18f7d63332a434c5e4a360d27991632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffe664db544dca84df6ffcfc11752c5",
+    "url": "/react_github_page/index.html"
+  },
+  {
+    "revision": "26873920f5f9c27ee2fc",
+    "url": "/react_github_page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b598fec17a24a1c4caac",
+    "url": "/react_github_page/static/js/2.8ca7348b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_github_page/static/js/2.8ca7348b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26873920f5f9c27ee2fc",
+    "url": "/react_github_page/static/js/main.bc2bee27.chunk.js"
+  },
+  {
+    "revision": "0ec8e7496e70368e549f",
+    "url": "/react_github_page/static/js/runtime-main.6c919ee0.js"
+  }
+]);
